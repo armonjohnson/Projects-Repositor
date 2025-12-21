3c9c813f7c62e13b0fced503a5ce79bc4c737bb2
armonjohnson <armon.johnson3434@gmail.com>
2025-12-21 11:49:25 -0600

    Update after_install.sh

--- projects/cloud-support-engineer-projects/project-2/scripts/after_install.sh
+++ projects/cloud-support-engineer-projects/project-2/scripts/after_install.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 set -e
 
-# folder where this script lives: .../project-2/scripts
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# project root: .../project-2
-PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
-
-# Nginx web root (adjust if yours is different)
 WEB_ROOT="/usr/share/nginx/html"
+SRC="/opt/app/frontend/build"
 
-echo "PROJECT_ROOT=$PROJECT_ROOT"
-echo "Copying build to $WEB_ROOT"
+echo "Copying $SRC to $WEB_ROOT"
 
-sudo rm -rf "$WEB_ROOT"/*
-sudo cp -R "$PROJECT_ROOT/frontend/build/"* "$WEB_ROOT/"
+rm -rf "$WEB_ROOT"/*
+cp -R "$SRC"/* "$WEB_ROOT"/
 
-sudo systemctl restart nginx
+systemctl restart nginx
